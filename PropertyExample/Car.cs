d91b8cd69c3bacaba2e490faad45439185a07d41
Allan Pead <adpead@gmail.com>
2024-02-08 11:49:46 +0200

    Initial Commit

---
+++ PropertyExample/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyExample
+{
+    internal class Car
+    {
+        public string Model { get; set; }
+
+        public int CurrentSpeed
+        {
+            get { 
+                return GetCurrentSpeed(); 
+            }
+            set {
+                SetCurrentSpeed(value);
+             
+            }
+        }
+
+
+
+
+
+        private int _currentSpeed;
+        private string _make;
+        //private string _model;
+        private int _topSpeed;
+       // public int CurrentSpeed;
+
+
+        public Car(string model, string make, int topSpeed)
+        {
+            _make = make;
+            Model = model;
+            _topSpeed = topSpeed;
+        }
+
+        public void SetCurrentSpeed(int currentSpeed)
+        {
+            if (currentSpeed > _topSpeed)
+            {
+                _currentSpeed = _topSpeed;
+            }
+            else
+                _currentSpeed = currentSpeed;
+        }
+
+        public int GetCurrentSpeed()
+        {
+            return _currentSpeed;
+        }
+
+    }
+}
---
+++ PropertyExample/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PropertyExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car fastCar = new Car("Indica", "Tata", 110);
+            fastCar.CurrentSpeed = 150;
+
+            Console.WriteLine(fastCar.Model);
+            Console.WriteLine(fastCar.CurrentSpeed);
+        }
+    }
+}
